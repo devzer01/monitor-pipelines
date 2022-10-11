2fd77ef379d51170f52df42d7d3f72fbfc61aae5
GitHub Action <action@github.com>
2022-10-11 12:04:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/devzer01/monitorpipelines/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/devzer01/monitorpipelines/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.devzer01.monitorpipelines.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.devzer01.monitorpipelines.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/devzer01/monitorpipelines/services/MyProjectService.kt
+++ src/main/kotlin/com/github/devzer01/monitorpipelines/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.devzer01.monitorpipelines.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.devzer01.monitorpipelines.MyBundle
 
 class MyProjectService(project: Project) {
 
